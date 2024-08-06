c1534cfe13db8b136fbca2d56759206e636688f0
Ahmed Mahmoud1P <109467491+AhemdMahmoud@users.noreply.github.com>
2024-08-06 21:14:54 +0300

    requirement  add

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # End2End-ObjectDetection
+
+## How to run the code
+1. Clone the repository
+2. Install the required packages using the following command:
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="signLanguage",
+    version="0.0.0",
+    author="Ahmed Mahmoud",
+    author_email="Ahmedmamoud148@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
+ 
