671202611f0de955274e566602b56ac3cac68968
Ahmed Mahmoud1P <109467491+AhemdMahmoud@users.noreply.github.com>
2024-08-14 15:21:34 +0300

    data ingestion add

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from signlanguage.logger import logging
-from signlanguage.exception import SignException
-import sys
+# from signlanguage.logger import logging
+# from signlanguage.exception import SignException
+# import sys
 
 
-# logging.info("Hello  ") 
+# # logging.info("Hello  ") 
 
-try :
-   a= 7/'9'
-except Exception as e:
-    raise SignException(e, sys) from e
+# try :
+#    a= 7/'9'
+# except Exception as e:
+#     raise SignException("Error occurred", sys) from e  
+
+
+from signlanguage.pipeline.training_pipeline import TrainPipeline
+
+obj = TrainPipeline()
+obj.run_pipeline() 
